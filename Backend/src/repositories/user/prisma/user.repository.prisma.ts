0d9ac7ac4fc49cfa1e1018e1e52925b3188ee643
felipe-py <luis.silvareal77@gmail.com>
2025-06-22 22:39:21 -0300

    feat(Backend): ORM user methods (save, update, find)

---
+++ Backend/src/repositories/user/prisma/user.repository.prisma.ts
@@ -0,0 +1,55 @@
+import { PrismaClient } from "@prisma/client";
+import { UserRepository } from "../user.repository";
+import { User } from "../../../entities/user";
+
+export class UserRepositoryPrisma implements UserRepository {
+    private constructor(readonly prisma: PrismaClient) {}
+
+    public static build(prisma: PrismaClient) {
+        return new UserRepositoryPrisma(prisma);
+    }
+
+    public async save(user: User): Promise<void> {
+        const data = {
+            id: user.id,
+            email: user.email,
+            password: user.password,
+            name: user.name,
+        };
+
+        await this.prisma.user.create({
+            data,
+        });
+    }
+
+    public async update(user: User): Promise<void> {
+        const data = {
+            id: user.id,
+            email: user.email,
+            password: user.password,
+            name: user.name,
+        };
+
+        await this.prisma.user.update({
+            where: {
+                id: user.id,
+            },
+            data,
+        });    
+    }
+
+    public async find(id: string): Promise<User | null> {
+        const aUser = await this.prisma.user.findUnique({
+            where: { id },
+        });
+
+        if(!aUser) { return null; }
+
+        const { email, password, name} = aUser;
+
+        const user = User.with(id, email, password, name);
+
+        return user
+    }
+
+}
