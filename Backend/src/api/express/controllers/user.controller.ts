ee5d03a04a61a7bf6579f755c95acfc517ad1079
felipe-py <luis.silvareal77@gmail.com>
2025-06-25 18:20:37 -0300

    feat: Adding valitations to create user

--- Backend/src/api/express/controllers/user.controller.ts
+++ Backend/src/api/express/controllers/user.controller.ts
@@ -2,6 +2,8 @@ import { Request, Response } from "express";
 import { UserRepositoryPrisma } from "../../../repositories/user/prisma/user.repository.prisma";
 import { prisma } from "../../../util/prisma.util";
 import { UserServiceImplementation } from "../../../services/user/implementation/user.service.implementation";
+import { validUser } from "../../../validators/user.validator";
+import { ZodError } from "zod";
 
 export class UserController {
     private constructor() {}
@@ -13,19 +15,31 @@ export class UserController {
     public async create(request: Request, response: Response) {
         const { email, password, name } = request.body;
 
-        const aRepository = UserRepositoryPrisma.build(prisma);
-        const aService = UserServiceImplementation.build(aRepository);
+        try {
 
-        const output = await aService.create(email, password, name)
+            validUser.parse({email, password, name})
 
-        const data = {
-            id: output.id,
-            email,
-            password,
-            name,
-        };
+            const aRepository = UserRepositoryPrisma.build(prisma);
+            const aService = UserServiceImplementation.build(aRepository);
 
-        response.status(201).json(data).send();
+            const output = await aService.create(email, password, name)
+
+            response.status(201).json(output).send();
+        
+        } catch (error) {
+
+            if (error instanceof ZodError) {
+                return response.status(400).json({
+                    message: "Erro de validação",
+                    errors: error.flatten().fieldErrors,
+                });
+            }
+
+            console.error(error); 
+            return response.status(500).json({
+                message: "Ocorreu um erro interno no servidor.",
+            });
+        }  
     }
     
 }
