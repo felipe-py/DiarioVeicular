ae20d3b4a0586d5e299bc937dc802bd404a45989
felipe-py <luis.silvareal77@gmail.com>
2025-07-17 00:09:20 -0300

    feat: Adicionada rota post (Cadastro de veículo)

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -1,13 +1,17 @@
 import { ApiExpress } from "./api/express/api.express";
+import { CarController } from "./api/express/controllers/car.controller";
 import { UserController } from "./api/express/controllers/user.controller";
+import { authMiddleware } from "./middleware/user.authenticate.middleware";
 
 function main () {
     const api = ApiExpress.build();
 
-    const controller = UserController.build();
+    const userController = UserController.build();
+    const carController = CarController.build();
 
-    api.addPostRoute("/users/register", controller.create);
-    api.addPostRoute("/users/login", controller.login)
+    api.addPostRoute("/users/register", userController.create);
+    api.addPostRoute("/users/login", userController.login);
+    api.addPostRoute("cars/register", authMiddleware, carController.create);
 
     api.start(8000);
 }
