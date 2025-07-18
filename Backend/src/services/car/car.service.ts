7ab683e0a62297bcc6eb256b826b836760d8ba39
felipe-py <luis.silvareal77@gmail.com>
2025-07-18 20:37:42 -0300

    feat: Adicionado service de exclusão do veículo do sistema

--- Backend/src/services/car/car.service.ts
+++ Backend/src/services/car/car.service.ts
@@ -6,17 +6,27 @@ export type CreateCarOutputDto = {
     manufacture_year: string;
     model_year: string;
     km: number;
-    onwer_id: string;
+    owner_id: string;
+    message: string;
+}
+
+export type DeleteCarOutputDto = {
+    car_license: string;
+    owner_id: string;
     message: string;
 }
 
 export interface CarService {
-    createCarService(car_license: string,
-    brand: string,
-    model: string,
-    color: string,
-    manufacture_year: string,
-    model_year: string,
-    km: number,
-    onwer_id: string): Promise<CreateCarOutputDto>;
+
+    createCarService(
+        car_license: string,
+        brand: string,
+        model: string,
+        color: string,
+        manufacture_year: string,
+        model_year: string,
+        km: number,
+        owner_id: string): Promise<CreateCarOutputDto>;
+
+    deleteCarService(car_license: string): Promise<DeleteCarOutputDto>;
 }
--- Backend/src/services/car/implementation/car.service.implementation.ts
+++ Backend/src/services/car/implementation/car.service.implementation.ts
@@ -1,6 +1,6 @@
 import { Car } from "../../../entities/car";
 import { CarRepository } from "../../../repositories/car/car.repository";
-import { CarService, CreateCarOutputDto } from '../car.service';
+import { CarService, CreateCarOutputDto, DeleteCarOutputDto } from '../car.service';
 
 export class CarServiceImplementation implements CarService{
 
@@ -10,16 +10,20 @@ export class CarServiceImplementation implements CarService{
         return new CarServiceImplementation(repository)
     } 
 
-    public async createCarService(car_license: string, brand: string, model: string, color: string, manufacture_year: string, model_year: string, km: number, onwer_id: string): Promise<CreateCarOutputDto> {
-        const aCar = Car.create(
-            car_license,
-            brand,
-            model,
-            color,
-            manufacture_year,
-            model_year,
-            km, onwer_id
-        )
+    public async createCarService(car_license: string, brand: string, 
+        model: string, color: string, manufacture_year: string, 
+        model_year: string, km: number, owner_id: string): Promise<CreateCarOutputDto> {
+        
+            const aCar = Car.create(
+                car_license,
+                brand,
+                model,
+                color,
+                manufacture_year,
+                model_year,
+                km, 
+                owner_id
+            )
 
         await this.repository.save(aCar);
 
@@ -31,8 +35,21 @@ export class CarServiceImplementation implements CarService{
             manufacture_year: aCar.carManufactureYear,
             model_year: aCar.carModelYear,
             km: aCar.carKm,
-            onwer_id: aCar.carOnwer,
+            owner_id: aCar.carOwner,
             message: "Veículo cadastrado com sucesso"
+        };
+
+        return output;
+    }
+
+    public async deleteCarService(car_license: string): Promise<DeleteCarOutputDto> {
+        
+        const deletedCar = await this.repository.delete(car_license);
+
+        const output: DeleteCarOutputDto = {
+            car_license: deletedCar.carLicense,
+            owner_id: deletedCar.carOwner,
+            message: "Registro do veículo excluído com sucessor"
         };
 
         return output;
