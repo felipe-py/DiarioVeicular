98e2e6a9c46e5225d0933d8b2904c4017aa3c649
felipe-py <luis.silvareal77@gmail.com>
2025-07-21 20:35:29 -0300

    feat: adicionado cors para comunicação com o frontend

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -2,12 +2,15 @@ import { ApiExpress } from "./api/express/api.express";
 import { CarController } from "./api/express/controllers/car.controller";
 import { UserController } from "./api/express/controllers/user.controller";
 import { authMiddleware } from "./middleware/user.authenticate.middleware";
+import cors from 'cors';
 
 const api = ApiExpress.build();
 const userController = UserController.build();
 const carController = CarController.build();
 
 export const app = api.expressApp;
+
+api.expressApp.use(cors());
 
 api.addPostRoute("/users/register", userController.create);
 api.addPostRoute("/users/login", userController.login);
