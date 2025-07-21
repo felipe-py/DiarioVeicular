792bcb21cff7fe28a45b125599af3f516515b985
felipe-py <luis.silvareal77@gmail.com>
2025-07-21 15:21:43 -0300

    cleanup: Comentários desnecessários

--- Backend/src/errors/carKm.error.invalid.ts
+++ Backend/src/errors/carKm.error.invalid.ts
@@ -2,7 +2,6 @@ import { AppError } from "./AppError";
 
 export class InvalidCarKm extends AppError {
     constructor(message: string) {
-        // Passamos a mensagem e o código de status HTTP 401 (Unauthorized)
         super(message, 400);
     }
 }
