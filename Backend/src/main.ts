d2d49d6444cab1c5cd212ef633f9f029ce8e14b7
felipe-py <luis.silvareal77@gmail.com>
2025-07-24 19:13:25 -0300

    feat: Adicionada rota de dashboard (Busca todos os carros do usuário logado)

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -2,7 +2,7 @@ import { ApiExpress } from "./api/express/api.express";
 import { CarController } from "./api/express/controllers/car.controller";
 import { UserController } from "./api/express/controllers/user.controller";
 import { authMiddleware } from "./middleware/user.authenticate.middleware";
-import cors from 'cors';
+import cors from "cors";
 
 const api = ApiExpress.build();
 const userController = UserController.build();
@@ -16,15 +16,20 @@ api.addPostRoute("/users/register", userController.create);
 api.addPostRoute("/users/login", userController.login);
 
 api.addPostRoute("/cars/register", authMiddleware, carController.create);
-api.addPatchRoute("/cars/update/:car_license", authMiddleware, carController.update);
+api.addPatchRoute(
+  "/cars/update/:car_license",
+  authMiddleware,
+  carController.update
+);
 api.addGetRoute("/cars/findCar", authMiddleware, carController.find);
+api.addGetRoute("/dashboard", authMiddleware, carController.findByOwner);
 api.addDeleteRoute("/cars/delete", authMiddleware, carController.delete);
 
 function startServer() {
-    const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 8000;
-    api.start(port);
+  const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 8000;
+  api.start(port);
 }
 
 if (require.main === module) {
-    startServer();
+  startServer();
 }
