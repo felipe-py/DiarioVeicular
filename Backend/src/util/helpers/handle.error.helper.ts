7bd9cd81b1ecd99aa5716be0fa3f5e5057fa1be1
felipe-py <luis.silvareal77@gmail.com>
2025-07-27 19:29:05 -0300

    refactor: Melhoria na lógica de condicionais

--- Backend/src/util/helpers/handle.error.helper.ts
+++ Backend/src/util/helpers/handle.error.helper.ts
@@ -5,67 +5,58 @@ import { NotFoundError } from "../../errors/error.NotFoundError";
 import { InvalidCarKm } from "../../errors/carKm.error.invalid";
 
 type output = {
-    status: number;
-    body: {
-        message: string,
-        errors?: any, 
-    };
+  status: number;
+  body: {
+    message: string;
+    errors?: any;
+  };
 };
 
-
 export function handleErrors(error: unknown): output {
-    
-    if (error instanceof ZodError) {
-        return {status: 400, 
-            body: {
-                message: "Erro de validação nos dados enviados.",
-                errors: error.flatten().fieldErrors,
-            },
-        };
-    };
-
-    if (error instanceof ConflictError) {
-        return {
-            status: error.statusCode,
-            body: {
-                message: error.message
-            }
-        }
-    }
-
-    if (error instanceof AuthenticationError) {
-        return {
-            status: error.statusCode,
-            body: {
-                message: error.message
-            }
-        }
-    }
-
-    if (error instanceof NotFoundError) {
-        return {
-            status: error.statusCode,
-            body: {
-                message: error.message
-            }
-        }
-    }
-
-    if (error instanceof InvalidCarKm) {
-        return {
-            status: error.statusCode,
-            body: {
-                message: error.message
-            }
-        }
-    }
-
-    console.error(error); 
-    
+  if (error instanceof ZodError) {
     return {
-        status: 500,
-        body: {
-            message: "Erro interno no servidor",
-        }
-    }
+      status: 400,
+      body: {
+        message: "Erro de validação nos dados enviados.",
+        errors: error.flatten().fieldErrors,
+      },
+    };
+  } else if (error instanceof ConflictError) {
+    return {
+      status: error.statusCode,
+      body: {
+        message: error.message,
+      },
+    };
+  } else if (error instanceof AuthenticationError) {
+    return {
+      status: error.statusCode,
+      body: {
+        message: error.message,
+      },
+    };
+  } else if (error instanceof NotFoundError) {
+    return {
+      status: error.statusCode,
+      body: {
+        message: error.message,
+      },
+    };
+  } else if (error instanceof InvalidCarKm) {
+    return {
+      status: error.statusCode,
+      body: {
+        message: error.message,
+      },
+    };
+  }
+
+  console.error(error);
+
+  return {
+    status: 500,
+    body: {
+      message: "Erro interno no servidor",
+    },
+  };
 }
