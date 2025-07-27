897e58a61e1944dae9c46c322766379135234f48
felipe-py <luis.silvareal77@gmail.com>
2025-07-27 18:08:37 -0300

    refactor: Adequando formatação do código

--- Backend/src/entities/user.ts
+++ Backend/src/entities/user.ts
@@ -1,49 +1,49 @@
 export type UserProps = {
-    id: string;
-    email: string;
-    password: string;
-    name: string;
+  id: string;
+  email: string;
+  password: string;
+  name: string;
 };
 
 export class User {
-    private constructor(readonly props: UserProps) {}
+  private constructor(readonly props: UserProps) {}
 
-    public static create(email: string, password: string, name: string) {
-        return new User({
-            id: crypto.randomUUID().toString(),
-            email,
-            password, 
-            name,
-        });
-    }
+  public static create(email: string, password: string, name: string) {
+    return new User({
+      id: crypto.randomUUID().toString(),
+      email,
+      password,
+      name,
+    });
+  }
 
-    public static with(
-        id: string,
-        email: string,
-        password: string,
-        name: string
-    ) {
-        return new User({
-            id,
-            email,
-            password,
-            name
-        });
-    }
+  public static with(
+    id: string,
+    email: string,
+    password: string,
+    name: string
+  ) {
+    return new User({
+      id,
+      email,
+      password,
+      name,
+    });
+  }
 
-    public get id() {
-        return this.props.id;
-    }
+  public get id() {
+    return this.props.id;
+  }
 
-    public get email() {
-        return this.props.email;
-    }
+  public get email() {
+    return this.props.email;
+  }
 
-    public get password() {
-        return this.props.password;
-    }
+  public get password() {
+    return this.props.password;
+  }
 
-    public get name() {
-        return this.props.name;
-    }
+  public get name() {
+    return this.props.name;
+  }
 }
