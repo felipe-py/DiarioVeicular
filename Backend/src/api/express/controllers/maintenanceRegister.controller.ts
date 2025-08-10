8300505b9c67685134dbdba0fb5cbfa92f001b35
felipe-py <luis.silvareal77@gmail.com>
2025-08-09 22:44:56 -0300

    feat: Esqueleto chamadas do controller

---
+++ Backend/src/api/express/controllers/maintenanceRegister.controller.ts
@@ -0,0 +1,17 @@
+export class MaintenanceRegisterController {
+  private constructor() {}
+
+  private static build() {
+    return new MaintenanceRegisterController();
+  }
+
+  public async create(request: Request, response: Response) {}
+
+  public async update(request: Request, response: Response) {}
+
+  public async find(request: Request, response: Response) {}
+
+  public async findByCar(request: Request, response: Response) {}
+
+  public async delete(request: Request, response: Response) {}
+}
