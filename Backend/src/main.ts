7eabb33dac015987ab08d15cb6cefc4b06eb536e
felipe-py <luis.silvareal77@gmail.com>
2025-08-10 17:19:24 -0300

    feat: Adding maintenance registers routes

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -1,5 +1,6 @@
 import { ApiExpress } from "./api/express/api.express";
 import { CarController } from "./api/express/controllers/car.controller";
+import { MaintenanceRegisterController } from "./api/express/controllers/maintenanceRegister.controller";
 import { UserController } from "./api/express/controllers/user.controller";
 import { authMiddleware } from "./middleware/user.authenticate.middleware";
 import cors from "cors";
@@ -7,6 +8,7 @@ import cors from "cors";
 const api = ApiExpress.build();
 const userController = UserController.build();
 const carController = CarController.build();
+const registerController = MaintenanceRegisterController.build();
 
 export const app = api.expressApp;
 
@@ -24,6 +26,32 @@ api.addPatchRoute(
 api.addGetRoute("/cars/findCar", authMiddleware, carController.find);
 api.addGetRoute("/dashboard", authMiddleware, carController.findByOwner);
 api.addDeleteRoute("/cars/delete", authMiddleware, carController.delete);
+
+api.addPostRoute(
+  "/maintenanceRegister",
+  authMiddleware,
+  registerController.create
+);
+api.addPatchRoute(
+  "/maintenanceRegister/update/:carLicense",
+  authMiddleware,
+  registerController.update
+);
+api.addGetRoute(
+  "/maintenanceRegister/findRegister/:maintenanceId",
+  authMiddleware,
+  registerController.find
+);
+api.addGetRoute(
+  "/maintenanceRegister/registerDashboard/:carLicense",
+  authMiddleware,
+  registerController.findByCar
+);
+api.addDeleteRoute(
+  "/maintenanceRegister/delete",
+  authMiddleware,
+  registerController.delete
+);
 
 function startServer() {
   const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 8000;
